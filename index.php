d8bb0950bfc1caed10cabe010ea0c4da4f4aab72
JosePaulCT <josepaulcamposterrones@gmail.com>
2024-05-06 00:10:51 -0500

    responsive

--- index.php
+++ index.php
@@ -24,16 +24,25 @@ curl_close($ch);
 <html>
 <head>
     <title>LA PROXIMA PELICULA DE MARVEL </title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    
 </head>
 
-<body class=" text-white  " style="background-image: url(<?= BG_URL; ?>); background-repeat: no-repeat ; background-size: cover" >
+<body class=" text-white" style="background-image: url(<?= BG_URL; ?>); background-repeat: no-repeat ; background-size: cover" >
+
+    <section class=" flex flex-col w-full h-screen justify-center items-center  ">
+    
     <p class="text-6xl text-center mt-5 font-mono font-extrabold  italic ">La siguiente pelicula de Marvel </p>
     <h1 class="text-5xl text-center mt-5 "><?php echo $data['title']; ?></h1>
-    <img src="<?= $data['poster_url']; ?> " alt="poster de la peli" width="200" class="mx-auto mt-5">
+    <img src="<?= $data['poster_url']; ?> " alt="poster de la peli" width="200" class="mx-auto mt-5 ">
 
-    <p class="text-3xl text-center mt-5  ">Se estrena el : </p>
+    <p class="text-3xl text-center mt-5 lg:text-white">Se estrena el : </p>
     <h1 class="text-2xl text-center mt-5  "><?php echo $data['release_date']; ?></h1>
-    <p class="text-xl text-center mt-2"> La proxima pelicula será: <a href="<?= $data['following_production']['poster_url'] ?> " target="_blank"> <?= $data['following_production']['title']  ?> </a> </p>
+    <p class="text-xl text-center text-white mt-2"> La proxima pelicula será: <a href="<?= $data['following_production']['poster_url'] ?> " target="_blank"> <?= $data['following_production']['title']  ?> </a> </p>
+    </section>
+
 </body>
+
+
 </html>
+
